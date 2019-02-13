75013dbcb9ce79f16c2f684c0079fc2ca02e3e1a
Sergio Toro <yiti007@gmail.com>
2019-02-13 15:33:43 +0100

    add sample app package

---
+++ packages/01-react/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Fact from "00-components";
+
+const Logo = styled.img`
+  width: 50px;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Logo src={logo} alt="logo" />
+          Cat facts
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
