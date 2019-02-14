f8006f8cd7a4a25b2e720ca4161d270207482647
Sergio Toro <yiti007@gmail.com>
2019-02-14 10:30:46 +0100

    allow sending parameters to api

--- packages/lambda-server/src/facts.js
+++ packages/lambda-server/src/facts.js
@@ -3,9 +3,10 @@ import { get } from "./utils/request";
 const BASE_URL = "https://cat-fact.herokuapp.com";
 
 export async function handler(event, context) {
-    const data = await get(`${BASE_URL}/facts/random?amount=5`);
-    return {
-        statusCode: 200,
-        body: data
-    };
+  const { types = "", amount = 5 } = event.queryStringParameters;
+  const data = await get(`${BASE_URL}/facts/random?amount=${amount}&animal_type=${types}`);
+  return {
+    statusCode: 200,
+    body: data
+  };
 }
--- packages/lambda-server/src/slow-facts.js
+++ packages/lambda-server/src/slow-facts.js
@@ -3,17 +3,18 @@ import { get } from "./utils/request";
 const BASE_URL = "https://cat-fact.herokuapp.com";
 
 function randomWait(maxWait = 5) {
-    return new Promise(resolve => {
-        const seconds = Math.floor(Math.random() * maxWait) + 1;
-        setTimeout(resolve, seconds * 1000);
-    });
+  return new Promise(resolve => {
+    const seconds = Math.floor(Math.random() * maxWait) + 1;
+    setTimeout(resolve, seconds * 1000);
+  });
 }
 
 export async function handler(event, context) {
-    const data = await get(`${BASE_URL}/facts/random?amount=5`);
-    await randomWait();
-    return {
-        statusCode: 200,
-        body: data
-    };
+  const { types = "", amount = 5 } = event.queryStringParameters;
+  const data = await get(`${BASE_URL}/facts/random?amount=${amount}&animal_type=${types}`);
+  await randomWait();
+  return {
+    statusCode: 200,
+    body: data
+  };
 }
