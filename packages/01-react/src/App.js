9504b49de04117cb6d3dc3d1e984b866614d0b8b
Sergio Toro <yiti007@gmail.com>
2019-02-15 13:24:24 +0100

    simplify starting step

--- packages/01-react/src/App.js
+++ packages/01-react/src/App.js
@@ -2,16 +2,7 @@
 import React, { Component, Fragment } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
-import {
-  ErrorMessage,
-  Button as BaseButton,
-  Fact as BaseFact,
-  Loading as BaseLoading,
-  SelectList as BaseSelectList,
-  RadioList as BaseRadioList,
-  CheckboxList as BaseCheckboxList,
-  defaultTheme
-} from "00-components";
+import { ErrorMessage, Button as BaseButton, Fact as BaseFact, Loading as BaseLoading, defaultTheme } from "00-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -29,20 +20,6 @@ GlobalStyle.defaultProps = {
 const Button = styled(BaseButton)`
   margin-left: 0;
   margin-right: 20px;
-`;
-
-const RadioList = styled(BaseRadioList)`
-  padding-right: 20px;
-  line-height: 40px;
-`;
-
-const CheckboxList = styled(BaseCheckboxList)`
-  padding-right: 20px;
-  line-height: 40px;
-`;
-
-const SelectList = styled(BaseSelectList)`
-  line-height: 40px;
 `;
 
 const Results = styled.div`
@@ -68,18 +45,12 @@ const Loading = styled(BaseLoading)`
 type State = {
   hasError: boolean,
   isLoading: boolean,
-  amount: number,
-  animals: Array<"cat" | "dog" | "snail" | "horse">,
-  lambdaFunction: "facts" | "slow-facts",
   facts: Object[]
 };
 class App extends Component<any, State> {
   state = {
     hasError: false,
     isLoading: false,
-    amount: 8,
-    lambdaFunction: "facts",
-    animals: ["cat"],
     facts: []
   };
 
@@ -90,11 +61,7 @@ class App extends Component<any, State> {
   async fetchFacts() {
     try {
       this.setState({ isLoading: true });
-      const { lambdaFunction, animals, amount } = this.state;
-      const types = animals.join(",");
-      const response = await fetch(
-        `https://kodify-workshop.netlify.com/.netlify/functions/${lambdaFunction}?amount=${amount}&types=${types}`
-      );
+      const response = await fetch(`https://kodify-workshop.netlify.com/.netlify/functions/facts?amount=6`);
       if (response.status === 200) {
         const facts = await response.json();
         this.setState({ facts, isLoading: false, hasError: false });
@@ -108,20 +75,8 @@ class App extends Component<any, State> {
     this.fetchFacts();
   };
 
-  onLambdaFunctionChanged = lambdaFunction => {
-    this.setState({ lambdaFunction }, () => this.fetchFacts());
-  };
-
-  onAnimalsChanged = animals => {
-    this.setState({ animals }, () => this.fetchFacts());
-  };
-
-  onAmountChanged = amount => {
-    this.setState({ amount }, () => this.fetchFacts());
-  };
-
   render() {
-    const { hasError, isLoading, facts, lambdaFunction, animals, amount } = this.state;
+    const { hasError, isLoading, facts } = this.state;
     return (
       <Fragment>
         <GlobalStyle />
@@ -130,38 +85,6 @@ class App extends Component<any, State> {
         </header>
 
         <Button onClick={this.onFetchFacts}>Refresh Facts</Button>
-        <RadioList
-          title="Endpoint:"
-          name="lambda"
-          value={lambdaFunction}
-          options={[{ value: "facts", text: "Facts" }, { value: "slow-facts", text: "Slow Facts" }]}
-          onChange={this.onLambdaFunctionChanged}
-        />
-        <CheckboxList
-          title="Animals:"
-          name="animals"
-          values={animals}
-          options={[
-            { value: "cat", text: "Cat" },
-            { value: "dog", text: "Dog" },
-            { value: "snail", text: "Snail" },
-            { value: "horse", text: "Horse" }
-          ]}
-          onChange={this.onAnimalsChanged}
-        />
-
-        <SelectList
-          title="Amount"
-          name="amount"
-          value={amount}
-          options={[
-            { value: 8, text: "8" },
-            { value: 16, text: "16" },
-            { value: 32, text: "32" },
-            { value: 64, text: "64" }
-          ]}
-          onChange={this.onAmountChanged}
-        />
 
         <Results>
           {hasError && <ErrorMessage>Error while fetching facts, try again later</ErrorMessage>}
