797545c5d8a69c045ac6b3dd0a29c3880c5e0a91
Sergio Toro <yiti007@gmail.com>
2019-02-18 13:44:24 +0100

    remove service worker and tests

--- packages/03-react-redux/src/index.js
+++ packages/03-react-redux/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import appReducer from "./modules/app";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(appReducer);
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
