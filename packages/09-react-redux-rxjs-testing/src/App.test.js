8302482531d6d7fde9dff0d1d7011efb15a629ea
Sergio Toro <yiti007@gmail.com>
2019-02-19 10:53:34 +0100

    add App test example

--- packages/09-react-redux-rxjs-testing/src/App.test.js
+++ packages/09-react-redux-rxjs-testing/src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from "./App";
+
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+const initialState = {
+  facts: {
+    hasError: false,
+    isLoading: false,
+    list: []
+  }
+};
+const store = mockStore(initialState);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
